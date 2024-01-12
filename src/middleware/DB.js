eec9bc695f710b9edbb0afdfd685a675b74944eb
juliochandra <juliochandra@gmail.com>
2024-01-13 03:39:20 +0700

    revert back to init

--- src/middleware/DB.js
+++
@@ -1,5 +0,0 @@
-const knex = require("knex");
-const knexConfig = require("../../knexfile");
-
-const DB = knex(knexConfig.development);
-module.exports = DB;
--- src/middleware/auth/Authenticate.js
+++
@@ -1,5 +0,0 @@
-const passport = require("./Passport");
-
-const authenticate = passport.authenticate("jwt", { session: false });
-
-module.exports = authenticate;
